5da4cdd36089cf89f40ee77d21da860ea76fa7cb
Shakil Nabil <shakilnabil11@gmail.com>
2025-01-14 23:05:26 -0500

    Initial commit

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+     return(
+          <header>
+               <h1>Study Hub</h1>
+               <nav>
+                    <ul>
+                         <li><a href="#">Home</a></li>
+                         <li><a href="#">Tools</a></li>
+                         <li><a href="#">About</a></li>
+                    </ul>
+               </nav>
+               <hr />
+          </header>
+     );           
+
+}
+
+export default Header
